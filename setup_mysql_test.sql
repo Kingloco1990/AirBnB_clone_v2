e874ea181caad68d5759aaa8d36365a443ad920f
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-01-24 15:25:42 +0000

    Add MySQL server setup script for hbnb_test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,17 @@
+-- This script prepares a MySQL server for the project.
+
+-- Check if the hbnb_test_db database exists; if not, create it.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Check if the hbnb_test user exists; if not, create it and set the password.
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the hbnb_test_db database to the hbnb_test user.
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the hbnb_test user.
+-- This allows hbnb_test to have SELECT privilege only on performance_schema.
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply the changes immediately.
+FLUSH PRIVILEGES;
