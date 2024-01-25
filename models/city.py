ebf54d66b37f1f36e15e67624191ffe3b99f141f
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-01-25 22:15:22 +0000

    Enhance City class with SQLAlchemy integration

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ 
+    The City class represents a city in the context of the AirBnB project.
+
+    Attributes:
+        __tablename__ (str): The name of the corresponding table in the database.
+        state_id (str): The ID of the state to which the city belongs.
+        name (str): The name of the city.
+    """
+    __tablename__ = 'cities'
+    state_id = Column(String(60), nullable=False, )
+    name = Column(String(128), ForeignKey("states.id"), nullable=False)
