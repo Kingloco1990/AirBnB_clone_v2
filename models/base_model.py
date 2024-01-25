ed3d7f9a2c7cfb01d6bbb270d28cbaa4659267db
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-01-25 21:59:21 +0000

    Update the BaseModel class to use SQLAlchemy

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,24 @@
 from uuid import uuid4
 from datetime import datetime
 from models import storage
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
 
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-
-        storage.new(self)
 
         if kwargs:
             for key, value in kwargs.items():
@@ -31,6 +38,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +49,23 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            dictionary.pop('_sa_instance_state', None)
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from storage.
+
+        This method removes the current instance from the storage system.
+
+        Note:
+            The deletion operation is performed using the `delete` method of the `storage` module.
+
+        Example:
+            To delete an instance named `my_instance`:
+
+            ```python
+            my_instance.delete()
+            ```
+        """
+        storage.delete(self)
