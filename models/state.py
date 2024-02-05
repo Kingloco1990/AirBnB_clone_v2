aec4a908c8961b193a4af81ead04534005a61cbb
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-02-05 15:13:16 +0000

    Refactor state.py for improved storage type handling

--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@ import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
+from os import getenv
 from models.city import City
 
 
@@ -13,18 +13,21 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship('City', backref='state', cascade='delete')
-    else:
+
+    if getenv("HBNB_TYPE_STORAGE") == "FileStorage":
         @property
         def cities(self):
-            """Getter attribute that returns a list of City instances associated with the current State.
+            """Getter attribute that returns a list of City instances
+               associated with the current State.
 
             Returns:
                 list: A list of City instances linked to the current State.
 
             Example:
-                To retrieve the cities associated with a State instance named 'my_state':
+                To retrieve the cities associated with a State instance
+                named 'my_state':
 
                 ```python
                 cities_list = my_state.cities
@@ -34,7 +37,8 @@ class State(BaseModel, Base):
             # Retrieve City objects (or instances) in storage.
             my_obj = models.storage.all(City)
 
-            # Filter City instances that have state_id equal to the current State's id.
+            # Filter City instances that have state_id equal to the
+            # current State's id.
             for obj in my_obj.values():
                 if obj.state_id == self.id:
                     my_list.append(obj)
