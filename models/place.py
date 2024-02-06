82663dcc5cb0ab95d2f07f95fa746e95d58d64f9
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-02-06 18:45:21 +0000

    Add relationship with Review model and property for reviews retrieval

--- models/place.py
+++ models/place.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from os import getenv
+from sqlalchemy.orm import relationship
+from models.review import Review
 
 
 class Place(BaseModel, Base):
@@ -18,3 +22,22 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    reviews = relationship('Review', backref='place', cascade='delete')
+
+if getenv("HBNB_TYPE_STORAGE") == "FileStorage":
+    @property
+    def review(self):
+        my_list = []
+        # Retrieve City objects (or instances) in storage.
+        my_obj = models.storage.all(Review)
+
+        # Filter Review instances that have place_id equal to the
+        # current Place's id.
+        for obj in my_obj.values():
+            if obj.place_id == self.id:
+                my_list.append(obj)
+
+        return (my_list)
