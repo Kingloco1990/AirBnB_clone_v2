f219a2cf44e4f2000b7e793d7650c8ef957aec2b
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-02-08 21:40:57 +0000

    Add SQLAlchemy functionality to Amenity model

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""Module defining the Amenity class for the AirBnB clone project."""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class representing amenities available in places.
+
+    Attributes:
+        __tablename__ (str): The table name for the database.
+        name (str): The name of the amenity.
+
+    Relationships:
+        - place_amenities: Many-to-many relationship with the Place class
+          through the PlaceAmenity association table.
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary='place_amenity')
