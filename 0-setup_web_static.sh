b531cb8aeaf2967342fb8a38996a1e10975d3973
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-30 15:59:32 +0000

    Sets up an Nginx server with custom configurations and content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -28,20 +28,18 @@ sudo tee /data/web_static/releases/test/index.html >/dev/null <<EOF
 EOF
 
 # Create symbolic link for web_static
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Change ownership of /data/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo wget -q -O /etc/nginx/sites-available/default http://exampleconfig.com/static/raw/nginx/ubuntu20.04/etc/nginx/sites-available/default
-config="/etc/nginx/sites-available/default"
-echo 'Holberton School Hello World!' | sudo tee /var/www/html/index.html > /dev/null
-sudo sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;}' $config
-sudo sed -i '/^}$/i \ \n\tlocation @404 {return 404 "Ceci n'\''est pas une page\\n";}' $config
-sudo sed -i 's/=404/@404/g' $config
-sudo sed -i "/^server {/a \ \tadd_header X-Served-By $HOSTNAME;" $config
-# Append location block to serve content from /data/web_static/current/
-# configure nginx
-sudo sed -i '61i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' $config
+# Define the path to the Nginx configuration file
+file="/etc/nginx/sites-available/default"
 
+# Append location block to serve content from /data/web_static/current/
+if ! grep -qF "location /hbnb_static" "$file"; then
+    sed -i '61i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' "$file"
+fi
+
+# Restart Nginx to apply the changes
 sudo service nginx restart
