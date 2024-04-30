180ee95ab09d6ba5a543fc5fe05d179f5154b3ba
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-30 14:16:11 +0000

    Sets up an Nginx server with custom configurations and content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -42,35 +42,6 @@ sudo ln -s "$target_folder" "$symbolic_link"
 # Change ownership of /data/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Set up default index page
-echo 'Hello World!' > /var/www/html/index.html
-
-# Create directory for custom error page
-sudo mkdir -p /var/www/error
-
-# Create custom 404 page
-echo "Ceci n'est pas une page" > /var/www/error/custom_404.html
-
-# Modify Nginx configuration to redirect '/redirect_me' to a YouTube video
-file="/etc/nginx/sites-available/default"
-sed -i "s/location \/ {/location \/redirect_me {/; s/try_files \$uri \$uri\/ =404;/return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;/" "$file"
-
-# Define the content to append
-# The content variable stores the text to be appended to the configuration file.
-content='\\n\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /var/www/error;\n\t\tinternal;\n\t}'
-
-# Use sed to append the content after the specified text in every location block in the file
-# Check if the specified text "error_page 404" is already present in the configuration file.
-# If not found, use sed to append the content variable after the closing brace of each location block in the file.
-if ! grep -qF "error_page 404" "$file"; then
-    sed '/^\s\+}/a'"$content" "$file"
-fi
-
-# Add a custom HTTP header with hostname as value
-if ! grep -qF "add_header X-Served-By" "$file"; then
-    sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\n\tadd_header X-Served-By $HOSTNAME;/" "$file"
-fi
-
 # Append location block to serve content from /data/web_static/current/
 if ! grep -qF "location /hbnb_static/" "$file"; then
     sed -i "s|internal;|internal;\n\t}\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;|" "$file"
