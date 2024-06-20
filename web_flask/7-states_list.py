0fef9ec3b390b049fa28d1dd3e762ceb85f45b16
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-06-20 20:26:55 +0000

    Add Flask web server for /states_list endpoint

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+
+This script defines a basic Flask web server that serves a list of states
+stored in a database when accessed at the /states_list URL.
+
+Usage:
+    Start the server by running this script. It will listen on all interfaces
+    (0.0.0.0) on port 5000.
+
+Endpoints:
+    /states_list: Displays a list of all State objects present in storage.
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """
+    Displays a list of all State objects.
+
+    This view function queries the storage for all State objects, then
+    renders a template to display the list of states.
+
+    Returns:
+        str: Rendered HTML template with the list of states.
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    Closes the storage on teardown.
+
+    This function is called after each request to close the storage,
+    ensuring any database connections are properly closed.
+
+    Args:
+        exception (Exception): The exception that caused the teardown,
+        if any.
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
