6c270b2af93a2b1eaebbc50a2ba11e2ff8185878
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-06-21 20:57:16 +0000

    Add Flask web server for /states_list endpoint

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,52 +1,64 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-
-This script defines a basic Flask web server that serves a list of states
-stored in a database when accessed at the /states_list URL.
-
-Usage:
-    Start the server by running this script. It will listen on all interfaces
-    (0.0.0.0) on port 5000.
-
-Endpoints:
-    /states_list: Displays a list of all State objects present in storage.
-"""
-from flask import Flask, render_template
+""" module doc """
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def state_list():
-    """
-    Displays a list of all State objects.
+@app.route("/", strict_slashes=False)
+def hello():
+    """ def doc """
+    return "Hello HBNB!"
 
-    This view function queries the storage for all State objects, then
-    renders a template to display the list of states.
 
-    Returns:
-        str: Rendered HTML template with the list of states.
-    """
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ def doc """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ def doc """
+    return 'c {}'.format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ def doc """
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ def doc """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ def doc """
+    if n % 2 == 0:
+        p = 'even'
+    else:
+        p = 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, parity=p)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ def doc """
     states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    Closes the storage on teardown.
-
-    This function is called after each request to close the storage,
-    ensuring any database connections are properly closed.
-
-    Args:
-        exception (Exception): The exception that caused the teardown,
-        if any.
-    """
+def close(error):
+    """ def doc """
     storage.close()
 
 
